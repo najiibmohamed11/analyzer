e3076a5ca78f7ce3bb7aacdcb94335b0e5fcb972
najiibmohamed11 <abdinajibmohamed304@gmail.com>
2025-12-08 23:24:55 +0300

    replacing chart colors

--- app/dashboard/components/EarningsChart.tsx
+++ app/dashboard/components/EarningsChart.tsx
@@ -223,27 +223,14 @@ export function ChartBarMultiple({transactions}:{transactions:transactionSchemaT
     data={transactionsDateType === "week" ? lastweekTransactions : lastMonthTransactions}
   >
 
-    <defs>
-      {/* Expense Gradient */}
-      <linearGradient id="expenseGradient" x1="0" y1="0" x2="0" y2="1">
-        <stop offset="0%" stopColor="oklch(0.75 0.17 27)" />
-        <stop offset="100%" stopColor="oklch(0.71 0.17 28)" />
-      </linearGradient>
-
-      {/* Income Gradient */}
-      <linearGradient id="incomeGradient" x1="0" y1="0" x2="0" y2="1">
-        <stop offset="0%" stopColor="oklch(0.85 0.14 130)" />
-        <stop offset="100%" stopColor="oklch(0.78 0.12 130)" />
-      </linearGradient>
-    </defs>
 
     <CartesianGrid vertical={false} />
     <XAxis dataKey="day" tickLine={false} tickMargin={10} axisLine={false} />
 
     <ChartTooltip cursor content={<ChartTooltipContent indicator="dashed" />} />
 
-    <Bar dataKey="expense" fill="url(#expenseGradient)" radius={4} />
-    <Bar dataKey="income" fill="url(#incomeGradient)" radius={4} />
+    <Bar dataKey="expense" fill="#F54927" radius={4} />
+    <Bar dataKey="income" fill="#076B1B" radius={4} />
 
   </BarChart>
 </ChartContainer>
